aac9bec81fb6b4d3cd9fc120d77796ed5d0afc15
jacoyutorius <jacoyutorius@gmail.com>
2022-09-07 03:50:04 +0900

    show message every 30 seconds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.png';
 import './App.css';
 
+async function Subscribe() {
+  console.log(`Hi, ${new Date()}`)
+
+  // 1分おきに実行
+  await new Promise(resolve => setTimeout(resolve, 1000 * 30)) 
+  await Subscribe();
+}
+
 function App() {
+  Subscribe()
+  
   return (
     <div className="App">
       <header className="App-header">
